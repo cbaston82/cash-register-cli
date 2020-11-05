6234c99b3453b1fc3e1bb2c5fbcbd3c5b298c2a1
Carlos Baston <carlosbaston82@gmail.com>
2020-11-04 16:31:51 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Cash Register Challenge (CLI)
+
+The CLI returns the change that is due along with the count of each denomination of bills and/or coins.
+
+## Installation
+
+Clone the project
+```bash
+git clone https://github.com/cbaston82/cash-register-cli.git
+```
+
+Navigate into project
+```bash
+cd cash-register-cli
+```
+
+Install dependencies
+```bash
+composer install
+```
+
+## Using the CLI
+
+Cash Register - change option
+```bash
+php cash-register change 100 10
+```
+![Change Option](https://res.cloudinary.com/imagine-design-develop/image/upload/v1604536885/screenshots/Xnip2020-11-04_16-39-17.png)
+
+Cash Register Help
+```bash
+php cash-register change -h
+```
+![Change -h](https://res.cloudinary.com/imagine-design-develop/image/upload/v1604537587/screenshots/Xnip2020-11-04_16-52-47.png)
---
+++ app/Commands/GetChange.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class GetChange extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'change {owed : The amount owed} {paying : The amount paying}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Gets the change from what is owed';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $drawer = array(10000, 5000, 2000, 1000, 500, 100, 25, 10, 5, 1);
+        $change = ($this->argument('owed') - $this->argument('paying')) * 100;
+        $result = array();
+
+        if($change !== 0) {
+            if ($change > 0) {
+                $this->info('Your change is ' . sprintf('$%0.2f', $change/100));
+
+                foreach ($drawer as $money){
+                    if ($change >= $money){
+                        $money_amount = floor($change/$money);
+                        $result[$money] = $money_amount;
+                        $change -= $money*$money_amount;
+                    }
+                }
+
+                foreach ($result as $dollar => $amount) {
+                    $this->info(sprintf('$%0.2f %d', $dollar/100, $amount));
+                }
+            } else {
+                $this->info('Your must pay less or what you owe. You cannot pay more!');
+            }
+        } else {
+            $this->info('You are all paid up. You get no change!');
+        }
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
